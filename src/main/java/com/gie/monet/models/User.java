436949173144b11d04fbb4478b0b8f96d2561af0
lonla <gatienjordanlonlaep@gmail.com>
2023-08-13 20:26:36 +0100

    user model design

--- src/main/java/com/gie/monet/models/User.java
+++ src/main/java/com/gie/monet/models/User.java
@@ -1,4 +1,17 @@
 package com.gie.monet.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String username;
+    private String firstName;
+    private String lastName;
 }
